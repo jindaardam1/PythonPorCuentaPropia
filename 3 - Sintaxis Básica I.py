0411553c16fbeb3a33b70f9dc3f8fed8a35d6b02
Jagoba Inda <jindaardam1@gmail.com>
2023-02-12 17:09:38 +0100

    Base

---
+++ 3 - Sintaxis Básica I.py
@@ -0,0 +1,12 @@
+print("Hola, nundo")
+
+print("Hola, mundo"); print("Adiós mundo cruel")
+# Poner varias instrucciones en una misma
+# fila es una práctica desaconsejable
+
+# Los comentarios se ponen con almohadilla.
+
+mi_nombre = "Mi nombre es Jagoba."
+mi_nombre = "Mi nombre " \
+            "es Jagoba"
+print(mi_nombre)
