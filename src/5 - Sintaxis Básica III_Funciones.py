8883329c6ca2c5620e81da6e82c2fcad2cdccb9a
Jagoba Inda <jindaardam1@gmail.com>
2023-02-13 03:09:00 +0100

    Vídeo 5

---
+++ src/5 - Sintaxis Básica III_Funciones.py
@@ -0,0 +1,11 @@
+def printear():
+    print("Estamos aprendiendo Python.")
+    print("Estamos aprendiendo instrucciones básicas.")
+    print("Poco a poco iremos avanzando.")
+
+
+printear()
+printear()
+printear()
+printear()
+printear()
