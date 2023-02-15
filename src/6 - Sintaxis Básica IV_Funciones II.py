36c2ddd7ca34f97e06bce4254b69f14c877f4eed
Jagoba Inda <jindaardam1@gmail.com>
2023-02-15 02:06:06 +0100

    Vídeo 5

---
+++ src/6 - Sintaxis Básica IV_Funciones II.py
@@ -0,0 +1,18 @@
+def sumadosvalores(num1, num2):
+    print(num1 + num2)
+
+
+sumadosvalores(3, 5)
+sumadosvalores(4, 38)
+sumadosvalores(9, 2)
+sumadosvalores(3.6, 9.2)
+
+
+def sumadosvaloresreturn(num1, num2):
+    return num1 + num2
+
+
+almacen = sumadosvaloresreturn(4, 9)
+
+
+print(almacen)
